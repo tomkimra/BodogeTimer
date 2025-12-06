2f882360771c131cc70c6394adc6890afff6de22
tomkimra <tomkimra@gmail.com>
2025-12-07 08:44:23 +0900

    feat:howler化

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -13,7 +13,7 @@ export default createVuetify({
       dark: {
         dark: true,
         colors: {
-          primary: '#1976D2',
+          primary: '#3f51b5',
           secondary: '#424242',
           accent: '#82B1FF',
         }
